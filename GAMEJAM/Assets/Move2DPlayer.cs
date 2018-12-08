19a48299916fd37797f6ed06e53435cfe184ffd8
MinKuk Jo <strike0115@naver.com>
2018-12-08 12:41:37 +0900

    Update from minkuk

---
+++ GAMEJAM/Assets/Move2DPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2DPlayer : MonoBehaviour {
+
+    public float Speed = 10.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * Speed);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * Speed);
+        }
+    }
+}
--- GAMEJAM/Assets/Scripts/Obj.cs
+++ GAMEJAM/Assets/Scripts/Obj.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class Obj : MonoBehaviour {
 
+    public GameObject twodemension;
+    public GameObject Player;
     public GameObject VFX;
     [HideInInspector]
    public GameObject VFXinstance;
     public OBJTYPE OBJTYPE;
-    public  virtual void OnGetThisObj()//클릭시
+
+	private void Start()
+	{
+        Player = GameObject.Find("Player");
+	}
+	public  virtual void OnGetThisObj()//클릭시
     {
         StageManager.instance.AddCountObj();
         VFXinstance= Instantiate(VFX, transform.position, transform.rotation);
+        Player.gameObject.transform.Find("Main Camera").GetComponent<GlitchEffect>().enabled = true;
         gameObject.SetActive(false);
-      
+        Invoke("OnMove", 3);
     }
-  
-	
+
+	public void OnMove()
+    {
+        twodemension.SetActive(true);
+        Player.SetActive(false);
+
+    }
+
+
 }
 public enum OBJTYPE { CLICK,TRIGGER}
